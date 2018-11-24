622738dbdbca9e40e8defe02ad9a245dd3484568
Emiliano A. Billi <emiliano.billi@gmail.com>
2018-11-24 17:36:58 -0300

    Update ERC20Interface.sol

--- contract/ERC20Interface.sol
+++ contract/ERC20Interface.sol
@@ -4,3 +4,5 @@ contract ERC20Interface {
     function balanceOf(address owner) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
 }
+
+
